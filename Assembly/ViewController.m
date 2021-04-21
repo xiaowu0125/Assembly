25f9ddbd2b4a027c3ed7d57fb020693e9e4516d5
fansen <sensen.fan@zhangmen.com>
2021-04-21 16:52:08 +0800

    测试使用

--- Assembly/ViewController.m
+++ Assembly/ViewController.m
@@ -15,7 +15,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UIButton *button = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    [self.view addSubview:button];
 }
 
 
